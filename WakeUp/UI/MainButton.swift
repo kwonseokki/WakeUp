2e06323ff5ba76a9507b03776cc8b31a230aae00
권석기 <kwons3327@gmail.com>
2025-11-14 02:56:55 +0900

    feat: ButtonStyle 추가

--- WakeUp/UI/MainButton.swift
+++ WakeUp/UI/MainButton.swift
@@ -7,9 +7,37 @@
 
 import SwiftUI
 
+enum ButtonStyle {
+    case `default`
+    case text
+    
+    var backgroundColor: Color {
+        switch self {
+        case .`default`:
+                .button
+        case .text:
+                .clear
+        }
+    }
+    
+    func textColor(disabled: Bool) -> Color {
+        switch (self, disabled) {
+        case (.default, false):
+                .brand
+        case (.default, true):
+                .neutralSecondary
+        case (.text, false):
+                .tertiary
+        case (.text, true):
+                .neutralTertiary
+        }
+    }
+}
+
 struct MainButton: View {
     let title: String
     var disabled: Bool = false
+    var buttonStyle: ButtonStyle = .default
     var action: (() -> ())?
     
     var body: some View {
@@ -18,11 +46,11 @@ struct MainButton: View {
         } label: {
             Text(title)
                 .font(.system(size: 17, weight: .semibold))
-                .foregroundStyle(.brand)
+                .foregroundStyle(buttonStyle.textColor(disabled: disabled))
                 .frame(maxWidth: .infinity, minHeight: 62)
         }
         .disabled(disabled)
-        .background(disabled ? .buttonDisasbled : .button)
+        .background(buttonStyle.backgroundColor)
         .cornerRadius(16)
     }
 }
